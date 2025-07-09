8471cba201bf7ad795e495cdaf009071f15202fb
Daniil Lohvinov <nsblnr@gmail.com>
2025-07-09 16:10:46 +0300

    refactor: bump libs ver, ua->uk locale fixes  [DEV-5377](https://webitel.atlassian.net/browse/DEV-5377)

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import en from './en/en';
 import ru from './ru/ru';
-import ua from './ua/ua';
+import uk from './uk/uk';
 
 Vue.use(VueI18n);
 
 const messages = {
   en,
   ru,
-  ua,
+  uk,
 };
 
 export default new VueI18n({
